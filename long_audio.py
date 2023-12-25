ecefff92669d847c5729437c03232d28b79499db
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2023-12-25 20:50:48 +0900

    Add prediction in test_load_model

--- long_audio.py
+++ long_audio.py
@@ -1,58 +1,66 @@
 import numpy as np
 import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+import numpy as np
 
-class Audio:
+class Long_audio:
     
     def __init__(self, path):
         self.path = path
-        self.sample_rate, self.original_data = wav.read(path)
-        data = self.original_data
-        if len(data.shape) > 1:
-            data = data.mean(axis=1)        
-        start, end = self.find_start_end(self.sample_rate, data)
-        self.trimmed_data = self.original_data[start:end]
+        self.sample_rate, self.data = wav.read(path)
+        if len(self.data.shape) > 1:
+            self.data = self.data.mean(axis=1)      
+        max_vol = np.max(np.abs(self.data))
+        self.threshold = 0.01 * max_vol
+        self.start_idx = []
+        self.end_idx = []
+        search_start_idx = 1
+        while(search_start_idx < len(self.data)):
+            search_start_idx = self.find_start_end(search_start_idx)
 
-    def find_start_end(self, sample_rate, data):
-        # ステレオの場合はモノラルに変換
+    def find_start_end(self, search_start_idx):
+        indices = np.where(np.abs(self.data[search_start_idx:]) >= self.threshold)[0]
+        if len(indices) > 0:
+            start_idx = indices[0] + search_start_idx
+            # start_idx = np.where(np.abs(self.data) >= self.threshold)[0][0]
+            # start_idx = np.where(np.abs(self.data[search_start_idx:]) >= self.threshold)[0][0] + search_start_idx
+            self.start_idx.append(start_idx)
+            # 186ミリ秒のサンプル数を計算
+            silence_length = int(0.186 * self.sample_rate)
+
+            # 終了位置を見つける
+            end_idx = len(self.data)
+            for i in range(start_idx + silence_length, len(self.data)):
+                if np.all(np.abs(self.data[i - silence_length:i]) < self.threshold / 10):
+                    end_idx = i - silence_length
+                    break
+            self.end_idx.append(end_idx)
+            return end_idx
+        else:
+            return len(self.data)
         
-        # 最大音量の10%を計算
-        max_vol = np.max(np.abs(data))
-        threshold = 0.1 * max_vol
-
-        # 開始位置を見つける
-        start_idx = np.where(np.abs(data) >= threshold)[0][0]
-
-        # 186ミリ秒のサンプル数を計算
-        silence_length = int(0.186 * sample_rate)
-
-        # 終了位置を見つける
-        end_idx = len(data)
-        for i in range(start_idx + silence_length, len(data)):
-            if np.all(np.abs(data[i - silence_length:i]) < threshold / 10):
-                end_idx = i - silence_length
-                break
-
-        return start_idx, end_idx
     
-    @staticmethod
-    def plot_waveform(data, title):
+    def print(self):
+        print(len(self.start_idx))
+        print(self.start_idx)
+        print(self.end_idx)
+    
+    def plot(self, title):
         plt.figure(figsize=(10, 4))
-        plt.plot(data)
+        plt.plot(self.data)
         plt.title(title)
         plt.xlabel('Samples')
         plt.ylabel('Amplitude')
+        for pt in self.start_idx:
+            plt.axvline(x=pt, color='r', linestyle='--')
+        for pt in self.end_idx:
+            plt.axvline(x=pt, color='r', linestyle='--')
+        plt.show()
 
-    def original_plot(self):
-        Audio.plot_waveform(self.original_data, "Original")
-
-    def trimmed_plot(self):
-        Audio.plot_waveform(self.trimmed_data, "Trimmed")
+    def predict(self):
         
-        
-
 if __name__ == "__main__":
-    wav1 = Audio('C:\\Users\\S2\\Documents\\デバイス作成\\2023測定デバイス\\swallowing\\dateset\\swallowing1.wav')
-    wav1.original_plot()
-    wav1.trimmed_plot()
-    plt.show()
+    wav1 = Long_audio('C:\\Users\\S2\\Documents\\デバイス作成\\2023測定デバイス\\swallowing\\test\\20231225\\test1.wav')
+    wav1.print()
+    wav1.plot("Test")
---
+++ test_load_model.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.models import load_model
+from dataset import DataSet
+import numpy as np
+from audio import Audio
+from wavelet import Wavelet
+import cv2
+
+if __name__ == "__main__":
+    loaded_model = load_model('20231225_159datasets.keras')
+
+    ##############  evaluateのやり方　##############
+    # directory_path = 'C:\\Users\\S2\\Documents\\デバイス作成\\2023測定デバイス\\swallowing\\dateset'
+    # test_voice_folder = directory_path + '\\shibata\\voice'
+    # test_cough_folder = directory_path + '\\shibata\\cough'
+    # test_swallowing_folder = directory_path + '\\shibata\\swallowing'   
+    # test_data = DataSet(9, 224, 224, 3, 3)
+    # test_data.folder_to_dataset(test_swallowing_folder, np.array([0, 0, 1]), 0)
+    # test_data.folder_to_dataset(test_cough_folder, np.array([0, 1, 0]), 1)
+    # test_data.folder_to_dataset(test_voice_folder, np.array([1, 0, 0]), 2)
+
+    # test_loss, test_accuracy  = loaded_model.evaluate(test_data.data, test_data.labels)
+    # print("Test Loss:", test_loss)
+    # print("Test Accuracy:", test_accuracy)
+
+    ##############  predictionのやり方　##############
+
+    wav1 = Audio('C:\\Users\\S2\\Documents\\デバイス作成\\2023測定デバイス\\swallowing\\dateset\\washino\\voice\\voice1.wav')
+    swallowing1 = Wavelet(wav1.sample_rate, wav1.trimmed_data, )
+    coefficients, _ =  swallowing1.generate_coefficients()
+    new_data = DataSet(1, 224, 224, 3, 0)
+    new_data.add_to_dataset(0, coefficients, 0)
+    print(new_data.data.shape)
+    predictions = loaded_model.predict(new_data.data)
+    predicted_classes = np.argmax(predictions, axis=1)
+    print("Predicted classes:", predicted_classes)
+    print("Predicted probabilities:", predictions)
+    class_names = ['swallowing', 'cough', 'voice']  # 例
+    predicted_class_names = [class_names[i] for i in predicted_classes]
+    print("Predicted class names:", predicted_class_names)
+
+
+
+
