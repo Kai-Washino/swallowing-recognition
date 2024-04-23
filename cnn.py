f0ec0fc9c76cbb4f1e388300b84757a72018378d
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-04-23 11:58:40 +0900

    Add time range

--- cnn.py
+++ cnn.py
@@ -7,10 +7,12 @@ from sklearn.preprocessing import StandardScaler
 from .dence_net import DanceNet
 
 class CNN(DanceNet): 
-    def __init__(self, dimension, num_class = 2):
-        self.num_class = num_class        
+    def __init__(self, scale = 127, time_range = 70000, num_class = 2):
+        self.num_class = num_class
+        self.scale = scale
+        self.time_range = time_range                
         self.model = tf.keras.models.Sequential([
-            Masking(mask_value=0.0, input_shape=(127, dimension)),            
+            Masking(mask_value=0.0, input_shape=(scale, time_range)),            
             Conv1D(32, 3, activation='relu'),  # 第1畳み込み層
             MaxPooling1D(2),  # 第1プーリング層
             Conv1D(64, 3, activation='relu'),  # 第2畳み込み層
@@ -42,19 +44,18 @@ if __name__ == "__main__":
     test_swallowing_folder = directory_path / 'shibata' / 'swallowing'    
     
     # train_data = VariableDataSet()
-    test_data = VariableDataSet()
+    test_data = VariableDataSet(num_samples=28, time_range=50000)
 
     # train_data.folder_to_dataset(train_swallowing_folder, np.array(0))
     # train_data.folder_to_dataset(train_cough_folder, np.array(1))    
     # train_data.folder_to_dataset(train_voice_folder, np.array([1, 0, 0]), 2)
     # train_data.print_label()
-    test_data.folder_to_dataset(test_swallowing_folder, np.array(0))
-    test_data.folder_to_dataset(test_cough_folder, np.array(1))
-    test_data.list_to_np()
+    test_data.folder_to_dataset(test_swallowing_folder, np.array(0), 0)
+    test_data.folder_to_dataset(test_cough_folder, np.array(1), 14)
 
     # test_data.folder_to_dataset(test_voice_folder, np.array([1, 0, 0]), 2)
 
-    model = CNN(test_data.max_cols)
+    model = CNN(time_range=50000)
     # model.training(train_data.data, train_data.labels, 1, 32)
     model.training(test_data.data, test_data.labels, 2, 32)
     # model.evaluate(test_data.data, test_data.labels)
