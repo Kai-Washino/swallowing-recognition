3d3391c4d4cbd67d9eec821ca73af95ef56f6ef5
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-05-07 15:06:02 +0900

    Update to split data

--- split_wav_file.py
+++ split_wav_file.py
@@ -33,9 +33,9 @@ def split_wav_file(filename, destination_folder, file_label, start_idx, chunk_le
 
 if __name__ == "__main__":
     ########## 変更する場所 ###########
-    action_name: str = "movement" # 何をしたのか？嚥下ならswallowing 咳ならcough等    
-    person_name: str = "ibuki"
-    recorded_wav_file_name: str= 'C:\\Users\\S2\\Documents\\デバイス作成\\2024測定デバイス\\swallowing\\original_data\\' + action_name +'_' + person_name + '.wav' # わけるwavファイルの名前
+    action_name: str = "cough" # 何をしたのか？嚥下ならswallowing 咳ならcough等    
+    person_name: str = "soichiro"
+    recorded_wav_file_name: str= 'C:\\Users\\S2\\Documents\\デバイス作成\\2024測定デバイス\\swallowing\\original_data\\' + action_name +'_' + person_name + '_1.wav' # わけるwavファイルの名前
     save_folder_name: str = "C:\\Users\\S2\\Documents\\デバイス作成\\2024測定デバイス\\swallowing\\dataset\\" + person_name + "\\" + action_name # 保存するフォルダの名前
     
     start_idx: int = 1 # wavファイルの名前の数字を何から始めるか？
